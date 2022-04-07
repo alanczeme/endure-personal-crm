e6d8e954934120c7e870f71935ca13a85320fcab
Alan Czemerinski <alanczeme@gmail.com>
2022-04-07 17:34:05 -0400

    add a few validations to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,8 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :contactevents
   has_many :contacts, through :contactevents
+
+  validates :title, presence: true
+  validates :start, presence: true
+  validates :end, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
     has_many :contacts
 
     has_secure_password
+
+    validates :username, presence: true, uniqueness: true
 end
