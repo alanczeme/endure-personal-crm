175f984392b7dd32f6a8ff38336e8055bcc4ff79
Alan Czemerinski <alanczeme@gmail.com>
2022-04-14 10:51:52 -0400

    add show method to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,15 @@ class EventsController < ApplicationController
     def index
         render json: Event.all
     end
+
+    def show
+        event = find_event
+        render json: event, status: :ok
+    end
+
+    private
+
+    def find_event
+        Event.find(params[:id])
+    end
 end
